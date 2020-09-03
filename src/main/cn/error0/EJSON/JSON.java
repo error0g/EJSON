0a8a882902ae6ee3a6b7cb78da8b84895b2d30f0
error0 <599727173@qq.com>
2020-09-03 17:44:30 +0800

    封装了parse方法

--- src/main/cn/error0/EJSON/JSON.java
+++ src/main/cn/error0/EJSON/JSON.java
@@ -1,11 +1,18 @@
 package cn.error0.EJSON;
 
-
+import cn.error0.EJSON.parser.JSONLexer;
+import cn.error0.EJSON.parser.JSONParser;
 
 public abstract class JSON {
 
-    public static JSONContainer parse(String text) {
-        JSONContainer e=null;
+    public static Object parse(String text) {
+        if(text==null) {
+            return null;
+        }
+        Object e=null;
+        JSONLexer lexer=new JSONLexer(text);
+        JSONParser JSONParser =new JSONParser(lexer);
+        e=JSONParser.stat();
         return e;
     }
 }
