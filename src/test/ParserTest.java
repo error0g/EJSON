fcef2e0493ea3814a43ae7f6bec1e86231122199
error0 <599727173@qq.com>
2020-09-03 17:42:34 +0800

    新测试样例

--- src/test/ParserTest.java
+++ src/test/ParserTest.java
@@ -1,32 +1,46 @@
 
+import cn.error0.EJSON.JSON;
+import cn.error0.EJSON.JSONArray;
+import cn.error0.EJSON.JSONContainer;
+import cn.error0.EJSON.parser.Token;
 import org.junit.Test;
-
 import cn.error0.EJSON.parser.JSONLexer;
-import cn.error0.EJSON.parser.JSONParser;
-import cn.error0.EJSON.parser.Lexer;
+
 
 public class ParserTest {
 
     @Test
     public void Example1()
     {
-        String json="[1,2,\":\"]";
-
-        Lexer lexer=new JSONLexer(json);
-        JSONParser JSONParser =new JSONParser(lexer);
-        JSONParser.stat();
-
+        String text="{\"Number\":[-1.1,2,3,100000000]}";
+        JSONContainer json= (JSONContainer) JSON.parse(text);
+        JSONArray array= (JSONArray) json.get("Number");
+        array.forEach(System.out::println);
     }
 
     @Test
    public void Example2()
     {
-        String json="{\"n\":1}";
-        Lexer lexer=new JSONLexer(json);
-        JSONParser JSONParser =new JSONParser(lexer);
-        JSONParser.stat();
+        String text="{\"boolean\":[null,false,true]}";
+
+        JSONContainer json= (JSONContainer) JSON.parse(text);
+        JSONArray array= (JSONArray) json.get("boolean");
+        array.forEach((item)->{
+            System.out.println(item);
+        });
 
     }
 
-
+    @Test
+    public void LexerTest()
+    {
+        String jsonstr="{\"n\":1.1}";
+        JSONLexer lexer=new JSONLexer(jsonstr);
+        Token token=lexer.NextToken();
+        while (token.getType()!=Token.TokenType.EOFTYPE)
+        {
+            System.out.println(token);
+            token=lexer.NextToken();
+        }
+    }
 }
