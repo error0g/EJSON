ba9fe47b001c59754b2b92bd1be52d9f31f8ed7d
error0 <599727173@qq.com>
2020-09-07 17:56:25 +0800

    添加防止重复引用机制

--- src/main/cn/error0/EJSON/JSONContainer.java
+++ src/main/cn/error0/EJSON/JSONContainer.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 
-public class JSONContainer<K,V> implements Map<K, V> {
+public class JSONContainer<K,V> extends JSON  implements Map<K, V>  {
 
     private final Map<K,V> map;
 
@@ -44,6 +44,7 @@ public class JSONContainer<K,V> implements Map<K, V> {
         return map.put(key,value);
     }
 
+
     @Override
     public V remove(Object key) {
         return map.remove(key);
@@ -72,5 +73,70 @@ public class JSONContainer<K,V> implements Map<K, V> {
     @Override
     public Set<Entry<K, V>> entrySet() {
         return map.entrySet();
+    }
+
+
+    @Override
+    public String toString() {
+        int index = 0;
+        res.put(this.hashCode(),"JSONContainer");
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append("{");
+        for (Map.Entry<K,V>  entry : map.entrySet()) {
+            String key= (String) entry.getKey();
+            Object value= entry.getValue();
+            stringBuilder.append("\"");
+            stringBuilder.append(key);
+            stringBuilder.append("\"");
+            stringBuilder.append(":");
+            if(value instanceof JSONContainer)
+            {
+                JSONContainer jsonContainer= (JSONContainer) value;
+                if(res.get(value.hashCode())!=null)
+                {
+                    res.put(value.hashCode(),"JSONContainer");
+                    stringBuilder.append(jsonContainer);
+                }
+                else {
+                    JSONContainer json=new JSONContainer();
+                    json.put("$ref","...");
+                    stringBuilder.append(json);
+                }
+            }
+            else if(value instanceof JSONArray)
+            {
+
+                JSONArray jsonArray= (JSONArray) value;
+                if(res.get(value.hashCode())==null)
+                {
+                    res.put(value.hashCode(),"JSONArray");
+                    stringBuilder.append(jsonArray);
+                }
+                else {
+                    JSONContainer jsonContainer=new JSONContainer();
+                    jsonContainer.put("$ref","...");
+                    stringBuilder.append(jsonContainer);
+                    stringBuilder.append("}");
+                }
+            }
+            else {
+                if(value instanceof String)
+                {
+                    stringBuilder.append("\"");
+                    stringBuilder.append(value);
+                    stringBuilder.append("\"");
+                }
+                else {
+                    stringBuilder.append(value);
+                }
+            }
+            if(index<map.size()-1)
+            {
+                stringBuilder.append(",");
+            }
+            ++index;
+        }
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
 }
