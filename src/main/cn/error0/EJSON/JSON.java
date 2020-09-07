d08510c40a8f73a9094693d7c0a12e9b6a6be0cd
error0 <599727173@qq.com>
2020-09-07 17:54:01 +0800

    添加引用map

--- src/main/cn/error0/EJSON/JSON.java
+++ src/main/cn/error0/EJSON/JSON.java
@@ -3,7 +3,12 @@ package cn.error0.EJSON;
 import cn.error0.EJSON.parser.JSONLexer;
 import cn.error0.EJSON.parser.JSONParser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class JSON {
+
+    public static final Map<Integer,String> res =new HashMap<>();
 
     public static Object parse(String text) {
         if(text==null) {
@@ -15,4 +20,5 @@ public abstract class JSON {
         e=JSONParser.stat();
         return e;
     }
+
 }
